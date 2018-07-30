7d6da5fcdad4e4610d3f1425c978f317a787cf94
Karan Navani <kknavani@gmail.com>
2018-07-30 12:12:55 +0100

    updated the create post page

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -7,7 +7,10 @@ function registrationsNew(req, res) {
 function registrationsCreate(req, res) {
   User
     .create(req.body)
-    .then(() => res.redirect('/posts'))
+    .then(user => {
+      req.session.userId = user.id;
+      res.redirect('/posts');
+    })
     .catch(() => res.status(500).redirect('/registrations/new'));
 }
 
